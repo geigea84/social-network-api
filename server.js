69fe20db212e1031985d7dd56bc014ffb1123812
geigea84 <geigea84@gmail.com>
2021-01-15 21:21:52 -0600

    added config/connection.js, set up connection.js and server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-//create a config/connection folder/file?
+const express = require("express");
+const app = express();
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(require("./routes"));
+
+//here or /config/connection.js?
+db.set("debug", true);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`Connected on localhost:${PORT}`);
+    });
+});
